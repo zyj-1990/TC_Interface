c037b4821a48f76cdb6735f00b0da9f8c44cbc1b
zyj-1990 <zhaoyanji@MacBook.local>
2016-06-28 00:23:20 +0800

    导入maven依赖jar包
    额外添加一个测试用例代码

--- src/test/java/tc/testcase/Login/UpdateLoginStatics.java
+++ src/test/java/tc/testcase/Login/UpdateLoginStatics.java
@@ -1,8 +1,63 @@
 package tc.testcase.Login;
 
+import net.sf.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import tc.config.ZhaoyanjiConfig;
+import tc.helper.CommonApi;
+import tc.utils.Entity;
+import tc.utils.Http;
+import tc.utils.HttpRequest;
+import tc.utils.Parameter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 /**
  * Created by zhaoyanji on 2016/6/27.
  */
-public class UpdateLoginStatics {
+public class UpdateLoginStatics extends ZhaoyanjiConfig{
+    @BeforeClass
+    public void beforeClass() {
 
+    }
+
+    @Test(dataProvider = "data")
+    public void updateLoginStatics(String msg,String user_account,String password,String version,String user_id,String exp_msg,int exp_code) throws Exception {
+        java.util.List<Parameter> headers = new ArrayList<Parameter>();
+        headers.add(new Parameter("Accept", "text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2"));
+        headers.add(new Parameter("Content-Type", "application/x-www-form-urlencoded"));
+
+        java.util.List<Parameter> paras = new ArrayList<Parameter>();
+        paras.add(new Parameter("user_account",user_account));
+        paras.add(new Parameter("password",password));
+        paras.add(new Parameter("version",version));
+        paras.add(new Parameter("user_id",user_id));
+
+        Http httpRequest = new Http("post", paras, headers, null);
+        JSONObject res = HttpRequest.sendRequest_EntityOrParas(httpRequest, host, "login/updateLoginStatics");
+        String err_msg = CommonApi.get_ErrorMsg(res);
+        int err_code = CommonApi.get_ErrorCode(res);
+        System.out.println(res);
+        Assert.assertEquals(err_msg,exp_msg,msg);
+        Assert.assertEquals(err_code,exp_code,msg);
+    }
+
+    @AfterClass
+    public void afterClass() {
+
+    }
+
+    @DataProvider
+    public Object[][] data(){
+        Object[][] data = null;
+        data = new Object[][]{
+                {"更新登录数据","13516810150","dc483e80a7a0bd9ef71d8cf973673924","100000",user_id,"success",0},
+        };
+        return data;
+    }
 }
