60834dc63d386b5049e281e652345d055f9fea02
zyj-1990 <zhaoyanji@MacBook.local>
2016-06-28 00:58:47 +0800

    测试方法

--- src/test/java/tc/testcase/user/GetCityList.java
+++ src/test/java/tc/testcase/user/GetCityList.java
@@ -1,7 +1,60 @@
 package tc.testcase.user;
 
+import net.sf.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import tc.config.ZhaoyanjiConfig;
+import tc.helper.CommonApi;
+import tc.utils.Http;
+import tc.utils.HttpRequest;
+import tc.utils.Parameter;
+
+import java.util.ArrayList;
+
 /**
  * Created by zhaoyanji on 6/24/16.
  */
-public class GetCityList {
+public class GetCityList extends ZhaoyanjiConfig{
+
+    @BeforeClass
+    public void beforeClass() {
+
+    }
+
+    @Test(dataProvider = "data")
+    public void getCityList(String msg,String user_account,String password,String version,String exp_msg,int exp_code) throws Exception {
+        java.util.List<Parameter> headers = new ArrayList<Parameter>();
+        headers.add(new Parameter("Accept", "text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2"));
+        headers.add(new Parameter("Content-Type", "application/x-www-form-urlencoded"));
+
+        java.util.List<Parameter> paras = new ArrayList<Parameter>();
+        paras.add(new Parameter("user_account",user_account));
+        paras.add(new Parameter("password",password));
+        paras.add(new Parameter("version",version));
+
+        Http httpRequest = new Http("post", paras, headers, null);
+        JSONObject res = HttpRequest.sendRequest_EntityOrParas(httpRequest, host, "user/getCityList");
+        String err_msg = CommonApi.get_ErrorMsg(res);
+        int err_code = CommonApi.get_ErrorCode(res);
+        System.out.println(res);
+        Assert.assertEquals(err_msg,exp_msg,msg);
+        Assert.assertEquals(err_code,exp_code,msg);
+    }
+
+    @AfterClass
+    public void afterClass() {
+
+    }
+
+    @DataProvider
+    public Object[][] data(){
+        Object[][] data = null;
+        data = new Object[][]{
+                {"获取验证码",user_account,"1","100000","success",0},
+        };
+        return data;
+    }
 }
--- src/test/java/tc/testcase/user/GetVerifyCode.java
+++ src/test/java/tc/testcase/user/GetVerifyCode.java
@@ -1,8 +1,62 @@
 package tc.testcase.user;
 
+import net.sf.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import tc.config.ZhaoyanjiConfig;
+import tc.helper.CommonApi;
+import tc.utils.Http;
+import tc.utils.HttpRequest;
+import tc.utils.Parameter;
+
+import java.util.ArrayList;
+
 /**
  * Created by zhaoyanji on 6/24/16.
  * 重新绑定获取验证码
  */
-public class GetVerifyCode {
+public class GetVerifyCode extends ZhaoyanjiConfig{
+
+    @BeforeClass
+    public void beforeClass() {
+
+    }
+
+    @Test(dataProvider = "data")
+    public void getVerifyCode(String msg,String mobile,String is_mobile_bind,String version,String code_type,String exp_msg,int exp_code) throws Exception {
+        java.util.List<Parameter> headers = new ArrayList<Parameter>();
+        headers.add(new Parameter("Accept", "text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2"));
+        headers.add(new Parameter("Content-Type", "application/x-www-form-urlencoded"));
+
+        java.util.List<Parameter> paras = new ArrayList<Parameter>();
+        paras.add(new Parameter("mobile",mobile));
+        paras.add(new Parameter("is_mobile_bind",is_mobile_bind));
+        paras.add(new Parameter("version",version));
+        paras.add(new Parameter("code_type",code_type));
+
+        Http httpRequest = new Http("post", paras, headers, null);
+        JSONObject res = HttpRequest.sendRequest_EntityOrParas(httpRequest, host, "findPwd/getVerifyCode");
+        String err_msg = CommonApi.get_ErrorMsg(res);
+        int err_code = CommonApi.get_ErrorCode(res);
+        System.out.println(res);
+        Assert.assertEquals(err_msg,exp_msg,msg);
+        Assert.assertEquals(err_code,exp_code,msg);
+    }
+
+    @AfterClass
+    public void afterClass() {
+
+    }
+
+    @DataProvider
+    public Object[][] data(){
+        Object[][] data = null;
+        data = new Object[][]{
+                {"获取验证码",user_account,"1","100000","3","success",0},
+        };
+        return data;
+    }
 }
